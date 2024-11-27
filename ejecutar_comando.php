9748a9d1badf9774a7564c1e04139a19084abb3e
mayjade002 <124312597+mayjade002@users.noreply.github.com>
2024-11-27 18:51:27 -0500

    Add files via upload

---
+++ ejecutar_comando.php
@@ -0,0 +1,15 @@
+<?php
+// Mostrar el nombre del host
+$hostname = gethostname();
+echo "<h1>Host: $hostname</h1>";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Información del Host</title>
+</head>
+<body>
+    <p>Esta página muestra el nombre del host de la máquina en la que se está ejecutando. testeo 1</p>
+</body>
+</html>
